c066b9afff04128a47b2cceadb32544bd8b8d638
Livy.Luu <113059423+LivyLuu@users.noreply.github.com>
2026-08-26 14:43:24 -0400

    intro cutscene facing direction

--- my-lovely-cafe/SCRIPTS/cutscene.gd
+++ my-lovely-cafe/SCRIPTS/cutscene.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
+var _rest_position: Vector3
+# spawn in facing marker 3d direction
+@onready var spawn_direction: Marker3D = $Player/starting_direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_rest_position = spawn_direction.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
