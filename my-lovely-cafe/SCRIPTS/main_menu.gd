801362c109ce5eb3df8686fd7246e480245cec35
Livy.Luu <113059423+LivyLuu@users.noreply.github.com>
2026-08-26 13:49:27 -0400

    new game button working

--- my-lovely-cafe/SCRIPTS/main_menu.gd
+++ my-lovely-cafe/SCRIPTS/main_menu.gd
@@ -1,1 +1,12 @@
 extends CanvasLayer
+
+@export_file("*.tscn") var new_game_scene: String = "res://! SCENES/! Game/!Start.tscn"
+
+func _on_btn_start_pressed() -> void:
+	# check if there are previous saves and play
+	
+	# start a new game
+	new_game()
+
+func new_game() -> void:
+	get_tree().change_scene_to_file(new_game_scene)
