6ceee8b1c13dfbff8bfc6073184127e08d352856
Livy.Luu <113059423+LivyLuu@users.noreply.github.com>
2026-08-27 03:02:19 -0400

    dialogue box updated to dialogue manager

--- my-lovely-cafe/SCRIPTS/Dialogue/dialogue_sequence.gd
+++ my-lovely-cafe/SCRIPTS/Dialogue/dialogue_sequence.gd
@@ -1,21 +1,15 @@
-# dialogue_sequence.gd
 extends Node3D
 class_name DialogueSequence
 
 @export var events: Array[DialogueEvent] = []
-@onready var dialogue_box: XRToolsViewport2DIn3D = $DialogueBox
+@onready var speaker_label: Label3D = $DialogueDisplay/SpeakerName
+@onready var dialogue_label: Label3D = $DialogueDisplay/DialogueText
 
 var _event_index: int = 0
 var _line_index: int = 0
-var _dialogue_label: Label
-var _speaker_label: Label
 
 
 func _ready() -> void:
-	var ui := dialogue_box.get_scene_instance()
-	_dialogue_label = ui.get_node("Control/MarginContainer2/VBoxContainer/dialogue_txt")
-	_speaker_label = ui.get_node("Control/MarginContainer/speaker_name")
-
 	_show_current_beat()
 
 
@@ -25,10 +19,9 @@ func _show_current_beat() -> void:
 
 	var event := events[_event_index]
 	if event is TextEvent:
-		_speaker_label.text = event.character_id
-		_dialogue_label.text = event.events[_line_index]
+		speaker_label.text = event.character_id
+		dialogue_label.text = event.events[_line_index]
 	elif event is OptionsEvent:
-		# build option buttons here later
 		pass
 
 
